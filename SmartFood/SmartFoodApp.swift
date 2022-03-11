2bb2cb4f5bf6d30da4bdcaaf6f26de0b5ba787b1
Francis Lavallee <francislavallee@hotmail.com>
2022-03-11 14:29:52 -0500

    Initial Commit

---
+++ SmartFood/SmartFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartFoodApp.swift
+//  SmartFood
+//
+//  Created by Francis Lavallée on 2022-03-11.
+//
+
+import SwiftUI
+
+@main
+struct SmartFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
